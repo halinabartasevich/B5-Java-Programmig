df35637a2333aa1eeb8872385be0462bab3cb7ab
halinabartasevich <hbartasevich@gmail.com>
2025-06-12 22:13:11 -0400

    new topic loop

---
+++ src/day17_loops/MaxAndMin.java
@@ -0,0 +1,18 @@
+package day17_loops;
+/*
+    write a program that asks user to enter 5 numbers and returns the biggest and smallest numbers from those 5
+    1
+    Ask a user:
+    Tell if it is biggest or smallest.
+
+    2
+    Ask a user:
+    Tell if it is biggest or smallest.
+
+    3
+    Ask a user:
+    Tell if it is biggest or smallest.
+ */
+public class MaxAndMin {
+
+}
