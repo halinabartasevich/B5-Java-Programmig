49dd49210065ce2456c644c8329ec0a06e5e8c30
halinabartasevich <hbartasevich@gmail.com>
2025-07-24 00:32:13 -0400

    new topic

---
+++ src/day34_c_inheritance/Cat.java
@@ -0,0 +1,20 @@
+package day34_c_inheritance;
+// Custom Class
+// Cat IS-A Animal
+public class Cat extends Animal {
+//    // Instance var
+//    String type;
+//    int numOfLegs;
+//    int age;
+//
+//    public void walk (){
+//        System.out.println(type + " is walking");
+//    }
+
+    // instance method
+    public void meow (){
+        System.out.println(type + " is meowing.");
+    }
+
+
+}
