65891665b258083d353c21c1d8503db107f951e1
halinabartasevich <hbartasevich@gmail.com>
2025-07-24 22:33:45 -0400

    bulk operations collections class

---
+++ src/day35_inheritance/person/Building.java
@@ -0,0 +1,7 @@
+package day35_inheritance.person;
+
+public class Building {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Tom";
+    }}
